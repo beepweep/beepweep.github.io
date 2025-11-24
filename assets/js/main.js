bdda12c3a9c7db6a1e1caba20dacc0a00a61e3a2
beepweep <madhur@beepweep.com>
2025-11-24 15:36:01 +0800

    Make footer year dynamic with automatic updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -96,3 +96,11 @@
     }
   });
 })();
+
+// Set current year in footer
+(function () {
+  const yearSpan = document.getElementById('current-year');
+  if (yearSpan) {
+    yearSpan.textContent = new Date().getFullYear();
+  }
+})();
